06e3ce1a582bc173c194a5a5ed3429ed447a23cc
Lauren Plenger <laurenb08@gmail.com>
2020-10-30 17:36:15 -0700

    restored timer function and fixed question display issue

--- assets/game.js
+++ assets/game.js
@@ -28,6 +28,7 @@ var questions = [
         correct: "answerThree"
     }
 ];
+console.log(questions);
 
 //event listener for button click
 document.getElementById("startButton").addEventListener("click", startQuiz());
@@ -35,30 +36,22 @@ document.getElementById("startButton").addEventListener("click", startQuiz());
 //This function runs when the start button is clicked
 function startQuiz() {
     //timer for quiz
-    var timerVar = setInterval(quizTimer, 1000) 
-        var timeLeft = 100;
-        function quizTimer() {
+    var timerVar = setInterval(quizTimer, 1000);
+    var timeLeft = 100;
+    function quizTimer() {
         timeLeft--,
-        document.getElementById("timer").textContent = timeLeft;
-        if (timeLeft = 0){
+        document.getElementById("timer").innerHTML = timeLeft;
+        if (timeLeft == 0){
             alert("GAME OVER");
             clearInterval(timerVar);
             return;
         }
-    } 1000;
+    }
 }
-
-
-
- //function to display questions
- displayQuestion();
-
- //function to display score
- displayScore();
 
 //This function displays the question
 function displayQuestion() {
-    var qNumber = questions(questionNumber);
+    var qNumber = questions[questionNumber];
 
     questionText.innerHTML = "<p>"+ qNumber.questionText +"</p>";
     choiceOne.innerHTML = "<p>"+ qNumber.choiceOne +"</p>";
@@ -76,6 +69,14 @@ function check() {
 function displayScore() {
     document.getElementById("finalScore") = timeLeft;
 }
+
+ //function to display questions
+ displayQuestion();
+
+ //function to display score
+ displayScore();
+
+console.log(questions);
 
 // WHEN I answer a question
 // THEN I am presented with another question
