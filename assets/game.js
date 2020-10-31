a6aaa6a8f81a74867fd26b47be479ddc41f88107
Lauren Plenger <laurenb08@gmail.com>
2020-10-30 18:23:31 -0700

    added js to track and store score

--- assets/game.js
+++ assets/game.js
@@ -4,7 +4,8 @@ var choiceTwo = document.getElementById("answerTwo");
 var choiceThree = document.getElementById("answerThree");
 var quizTimer = document.getElementById("timer");
 var questionNumber = 0;
-// var timeLeft = 100;
+var timeLeft = 100;
+var practiceScore = 75;
 
 //variable array to store questions and answers
 var questions = [
@@ -37,7 +38,6 @@ document.getElementById("startButton").addEventListener("click", startQuiz());
 function startQuiz() {
     //timer for quiz
     var timerVar = setInterval(quizTimer, 1000);
-    var timeLeft = 100;
     function quizTimer() {
         timeLeft--,
         document.getElementById("timer").innerHTML = timeLeft;
@@ -52,39 +52,49 @@ function startQuiz() {
 //This function displays the question
 function displayQuestion() {
     var qNumber = questions[questionNumber];
-
-    questionText.innerHTML = "<p>"+ qNumber.questionText +"</p>";
-    choiceOne.innerHTML = "<p>"+ qNumber.choiceOne +"</p>";
-    choiceTwo.innerHTML = "<p>"+ qNumber.choiceTwo +"</p>";
-    choiceThree.innerHTML = "<p>"+ qNumber.choiceThree +"</p>";
+        questionText.innerHTML = "<p>"+ qNumber.questionText +"</p>";
+        choiceOne.innerHTML = "<p>"+ qNumber.choiceOne +"</p>";
+        choiceTwo.innerHTML = "<p>"+ qNumber.choiceTwo +"</p>";
+        choiceThree.innerHTML = "<p>"+ qNumber.choiceThree +"</p>";
 }
 
 //This function checks if your answer is correct
 function check() {
-    if ( answer /= questions[qNumber].correct);
+    if ( answer != questions[qNumber].correct);
     timeLeft-5;
 }
 
 //This function displays your score
 function displayScore() {
-    document.getElementById("finalScore") = timeLeft;
+    document.getElementById("finalScore") = timeLeft.value;
 }
 
- //function to display questions
- displayQuestion();
+//this function saves and stores your score
+function saveScore(){
 
- //function to display score
- displayScore();
+    console.log("submitScore");
+    var initials = document.getElementById("initials");
+    console.log("initials".value);
 
-console.log(questions);
+    var currentScore = {
+        playerScore: timeLeft,
+        playerInitials: initials.value
+    }
 
-// WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
-// THEN the game is over
-// WHEN the game is over
-// THEN I can save my initials and score
+    console.log(currentScore);
 
+    var highScores = JSON.parse(window.localStorage.getItem("highScoreList")) || [];
+
+    highScores.push(currentScore);
+    localStorage.setItem("highScoreList", JSON.stringify(currentScore));
+}
+
+//event listener for startGame button click
+document.getElementById("startButton").addEventListener("click", startQuiz());
+
+//add event listener for submitScore button click
+document.getElementById("submitScore").addEventListener("click", saveScore);
+
+displayQuestion();
+displayScore();
 
